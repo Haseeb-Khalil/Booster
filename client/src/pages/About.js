f5b162ab1edf6c6ce4f03718403255f030ee0dd9
Luke Rogerson <contact@lukerogerson.me>
2022-03-05 12:53:37 +0000

    Test commit

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,7 @@
 const About = () => (
 	<main role="main">
 		<div>
-			<h1>About</h1>
+			<h1>About!!!</h1>
 			<p>
 				Starter kit for full-stack JavaScript projects. For more information,
 				see the wiki:
