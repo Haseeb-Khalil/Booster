097ffe0f15d8628390d37a731a616932f0fc15eb
DESKTOP-PRCDNMQ\Adem <ademalagoz46@gmail.com>
2022-03-05 13:20:53 +0000

    test for Adem

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
-
+//Test for Adem
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
