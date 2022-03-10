61c8edb9851afa1c2e9330b6e69ddf7b9e980bff
haseeb3927 <haseeb3927@gmail.com>
2022-03-10 15:53:21 +0000

    Added Get and Post request
    Added methods to get an energiser by id params, search query and also added a post method of adding energiser

--- server/api.js
+++ server/api.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const router = Router();
 
+<<<<<<< Updated upstream
 //load the booster JSON
 //const booster = require("../boosterData.json");
 
@@ -30,9 +31,118 @@ router.get("/energisers", (req, res) => {
     }).catch((err) => {
         console.log(err);
         res.status(500).send(err);
+=======
+// router.get("/", (_, res) => {
+// 	res.json({ message: "Hello, world!" });
+// });
+
+
+///=================POST REQUESTS==================///
+
+//  ADD AN ENERGISER
+router.post("/energisers", function (req, res) {
+  let energiserTitle = req.body.title;
+  let energiserImage = req.body.image;
+  let energiserDescription = req.body.description;
+  let energiserLink = req.body.link;
+
+//    || !energiserImage || !energiserLink
+
+  if (!energiserTitle  || !energiserDescription) {
+    return res
+      .status(400)
+      .send({ msg: "Please fill in all the required fields" });
+  }
+  pool
+    .query(`SELECT title FROM energisers WHERE title = $1`, [energiserTitle])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return res
+          .status(400)
+          .send({ msg: `Energiser name:${energiserTitle} already exist` });
+      } else {
+        let query = `INSERT INTO energisers(title, image, description, link) VALUES ($1,$2,$3,$4)`;
+        let params = [
+					energiserTitle,
+					energiserImage,
+					energiserDescription,
+					energiserLink,
+				];
+        pool
+          .query(query, params)
+          .then(() =>
+            res.send({ msg: `Energiser ${energiserName} added Successfully` })
+          )
+          .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+          });
+      }
     });
 });
 
 
 
+///==================GET REQUESTS===================///
+
+// GET ALL THE Energisers
+router.get("/energisers", function (req, res) {
+  pool
+    .query("SELECT * FROM energisers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+// GET ENERGISERS USING SEARCH
+router.get("/energisers/search", function (req, res) {
+
+  let searchQuery = req.query.term;
+
+  if (searchQuery) {
+    pool
+      .query(
+        `SELECT * WHERE LOWER(title) LIKE LOWER('%${searchQuery}%') ORDER BY title`
+      )
+      .then((result) => res.json(result.rows))
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json(error);
+      });
+  }
+});
+
+
+// GET ENERGISER WITH AN ID
+router.get("/energisers/:energiserId", function (req, res) {
+  let energiserId = req.params.energiserId;
+  let query = `SELECT * FROM energisers WHERE id = $1`;
+  const params = [energiserId];
+
+  pool
+    .query(query, params)
+    .then((result) => {
+      if (result.rows.length == 0) {
+        return res
+          .status(404)
+          .send({ msg: `Energiser: ${energiserId} doesn't exist` });
+      }
+      res.json(result.rows);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+>>>>>>> Stashed changes
+    });
+});
+
+
+
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 export default router;
