5623602f4353777ba40050d40d29e6c1217c9855
justsealan <justsealan@gmail.com>
2022-03-17 13:02:07 +0000

    Remove comments

--- client/src/components/Jumbotron/Registration.js
+++ client/src/components/Jumbotron/Registration.js
@@ -43,7 +43,6 @@ function Registration (){
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		const data = new FormData(event.currentTarget);
-		// eslint-disable-next-line no-console
 		console.log({
 			email: data.get("email"),
 			password: data.get("password"),
