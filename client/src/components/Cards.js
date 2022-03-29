da2daa31f58fecf35d194f162b5434268c1185d4
justsealan <justsealan@gmail.com>
2022-03-30 00:55:18 +0100

    Style search bar

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,29 +1,23 @@
+import React, { useState } from "react";
+import { Button, Link, Box, Typography, Grid } from "@mui/material";
+import { Card, CardContent, CardMedia, CardActions } from "@mui/material";
 
-import React,{ useState } from "react";
-import Card from "@material-ui/core/Card";
-import {
-	Button,
-	CardContent,
-	CardMedia,
-	Typography,
-	Grid,
-} from "@material-ui/core";
-import { CardActions } from "@mui/material";
 import Vote from "../components/CardsCarousel/Vote";
 import SearchBar from "../components/SearchBar/SearchBar";
+import { red } from "@mui/material/colors";
 
-function Cards( { listEnergisers } ) {
-  const [search, setSearch] = useState("");
-    let filteredEnergisers = listEnergisers.filter((energiser) => {
-			return (
-				energiser.title.toLowerCase().includes(search.toLowerCase()) ||
-				energiser.description.toLowerCase().includes(search.toLowerCase())
-			);
-		});
+function Cards({ listEnergisers }) {
+	const [search, setSearch] = useState("");
+	let filteredEnergisers = listEnergisers.filter((energiser) => {
+		return (
+			energiser.title.toLowerCase().includes(search.toLowerCase()) ||
+			energiser.description.toLowerCase().includes(search.toLowerCase())
+		);
+	});
 	return (
 		<>
 			<Grid component="main">
-				<SearchBar search={search} setSearch={setSearch} />
+				<SearchBar search={search} setSearch={setSearch} sx={{ mb: "10px" }} />
 				<Grid container spacing={5}>
 					{filteredEnergisers.map((energiser, index) => (
 						<Grid item key={index} xs={4} md={4}>
@@ -50,7 +44,7 @@ function Cards( { listEnergisers } ) {
 									<Button
 										variant="outlined"
 										href={`/energiser/${energiser.id}`}
-										color="primary"
+										sx={{ color: red[800] }}
 									>
 										Host now
 									</Button>
