cab25728d41681b1d903f67d1ea950517f58df7d
justsealan <justsealan@gmail.com>
2022-03-30 01:14:45 +0100

    Change footer style

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,80 +1,93 @@
 import React from "react";
-import { Container, Box, Grid, Link, Typography } from "@material-ui/core";
+import { Button, Link, Box, Typography, Container, Grid } from "@mui/material";
+
 import { red } from "@mui/material/colors";
 import ElectricBoltRoundedIcon from "@mui/icons-material/ElectricBoltRounded";
 
-export default function Footer(){
-    return (
-			<footer>
-				<Box
-					px={{ xs: 3, sm: 10 }}
-					py={{ xs: 5, sm: 10 }}
-					bgcolor="primary.main"
-					color="secondary.main"
-				>
-					<Container maxWidth="lg">
-						<Grid container spacing={5}>
-							<Grid item xs={12} sm={4}>
-								<Box borderBottom={1}>Help</Box>
-								<Box>
-									<Link href="/" color="inherit" variant="h6">
-										Contact
-									</Link>
-								</Box>
-								<Box>
-									<Link href="/" color="inherit" variant="h6">
-										Support
-									</Link>
-								</Box>
-								<Box>
-									<Link href="/" color="inherit" variant="h6">
-										Privacy
-									</Link>
-								</Box>
-							</Grid>
-							<Grid item xs={12} sm={4}>
-								<Box borderBottom={1} variant="h6">
-									Account
-								</Box>
-								<Box>
-									<Link href="/" color="inherit" variant="h6">
-										Login
-									</Link>
-								</Box>
-								<Box>
-									<Link href="/" color="inherit" variant="h6">
-										Register
-									</Link>
-								</Box>
-							</Grid>
-							<Grid item xs={12} sm={4}>
-								<Box borderBottom={1}>Github</Box>
-								<Box>
-									<Link href="/" color="inherit" variant="h6">
-										Team
-									</Link>
-								</Box>
-								<Box>
-									<Link href="/" color="inherit" variant="h6">
-										Resources
-									</Link>
-								</Box>
-							</Grid>
+export default function Footer() {
+	return (
+		<footer>
+			<Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }} bgcolor="primary">
+				<Container maxWidth="lg">
+					<Grid container spacing={5}>
+						<Grid item xs={12} sm={4}>
+							<Box borderBottom={1}>
+								<Typography variant="h6" color="secondary">
+									Help
+								</Typography>
+							</Box>
+							<Box>
+								<Link href="/" color="inherit" variant="h6">
+									<Typography variant="body1" color="secondary">
+										faq
+									</Typography>
+								</Link>
+							</Box>
+							<Box>
+								<Link href="/" color="inherit" variant="h6">
+									<Typography variant="body1" color="secondary">
+										privacy
+									</Typography>
+								</Link>
+							</Box>
 						</Grid>
-						<Box textAlign="center" pt={{ xs: 1, sm: 1 }} pb={{ xs: 5, sm: 0 }}>
-							<ElectricBoltRoundedIcon
-								sx={{ color: red[150] }}
-								fontSize="large"
-							/>
-							<Link color="secondary" variant="h4" href="./" underline="none">
+						<Grid item xs={12} sm={4}>
+							<Box borderBottom={1} variant="h6">
+								<Typography variant="h6" color="secondary">
+									Start
+								</Typography>
+							</Box>
+							<Box>
+								<Typography variant="body1" color="secondary">
+									host a game
+								</Typography>
+							</Box>
+							<Box>
+								<Typography variant="body1" color="secondary">
+									join
+								</Typography>
+							</Box>
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<Box borderBottom={1}>
+								<Typography variant="h6" color="secondary">
+									About
+								</Typography>
+							</Box>
+							<Box>
+								<Link href="/" color="inherit" variant="h6">
+									<Typography variant="body1" color="secondary">
+										team
+									</Typography>
+								</Link>
+							</Box>
+							<Box>
+								<Link href="/" color="inherit" variant="h6">
+									<Typography variant="body1" color="secondary">
+										github
+									</Typography>
+								</Link>
+							</Box>
+						</Grid>
+					</Grid>
+					<Box textAlign="center" pt={{ xs: 1, sm: 1 }} pb={{ xs: 5, sm: 0 }}>
+						<ElectricBoltRoundedIcon
+							sx={{ color: red[800] }}
+							fontSize="large"
+						/>
+						<Box>
+							<Link href="./" underline="none" sx={{ color: red[800] }}>
 								booster &reg; {new Date().getFullYear()}
 							</Link>
 						</Box>
-						<Box textAlign="center">
-							<Typography >Efortless Energiser</Typography>
-						</Box>
-					</Container>
-				</Box>
-			</footer>
-		);
+					</Box>
+					<Box textAlign="center">
+						<Typography variant="h5" color="secondary">
+							Efortless Energiser
+						</Typography>
+					</Box>
+				</Container>
+			</Box>
+		</footer>
+	);
 }
