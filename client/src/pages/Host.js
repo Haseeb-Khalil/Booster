16722d2f4dd191daa053f672eed1ebe6ce0e91db
justsealan <justsealan@gmail.com>
2022-04-01 03:45:38 +0100

    Style host page

--- client/src/pages/Host.js
+++ client/src/pages/Host.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import { useParams } from "react-router-dom";
 import { Button, Link, Box, Typography, Grid, Divider } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
@@ -9,11 +10,14 @@ import Timer from "../components/Timer";
 import Vote from "../components/CardsCarousel/Vote";
 
 const Host = ({ onlineCount }) => {
-	console.log("Hosting");
-	console.log(onlineCount);
+	const [value, setValue] = useState("My copy text");
+	const [copied, setCopied] = useState(false);
+	const textAreaRef = useRef(null);
+
+	const supervillains = require("supervillains");
+	const username = supervillains.random();
 	const [game, setGame] = useState();
 	const { id } = useParams();
-	console.log(id);
 	const api = process.env.API_URL || "/api";
 	useEffect(() => {
 		fetch(`${api}/game/${id}`, {
@@ -36,58 +40,104 @@ const Host = ({ onlineCount }) => {
 			});
 	}, [id]);
 
+
 	return game ? (
 		<Box>
 			<Header />
-
-			<Box bgcolor="primary">
-				<Grid container>
-					<Grid item xs={12}>
+			<Box
+				sx={{
+					backgroundImage:
+						"url(https://images.unsplash.com/photo-1495366554757-8568e69d7f80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80)",
+					backgroundSize: "cover",
+				}}
+			>
+				<Grid
+					container
+					flexDirection="column"
+					alignContent="center"
+					alignItems="center"
+				>
+					<Grid
+						container
+						item
+						xs={12}
+						style={{ gap: "1rem" }}
+						alignItems="baseline"
+					>
+						<Grid item xs={2} sx={{ ml: "2em", mt: "5em" }}>
+							<Timer duration={900} remaining={game.secondsLeft} />
+						</Grid>
 						<Grid
 							item
-							xs={11}
-							style={{ display: "flex", gap: "1rem", alignItems: "center" }}
-						>
-							<Box xs={2} sx={{ ml: "10px", mt: "5px" }}>
-								<Timer duration={900} remaining={game.secondsLeft} />
-							</Box>
-							<Box>
-								<h3>{onlineCount} - Users are Online</h3>
-							</Box>
-							<Box sx={{ ml: "auto", mr: "auto" }} textAlign="center">
-								<Typography variant="h3">{game.title}</Typography>
-							</Box>
-						</Grid>
-						<Box textAlign="center" sx={{ m: 6 }}>
-							<img src={game.image} height="480px" alt={game.title} />
-						</Box>
-						<Box
+							xs={4}
+							sx={{ ml: "auto", mr: "auto" }}
 							textAlign="center"
-							sx={{ mr: "auto", ml: "auto", maxWidth: "50em" }}
 						>
-							<Typography variant="h4">{game.description}</Typography>
-						</Box>
+							<Typography variant="h3">{game.title}</Typography>
+						</Grid>
+						<Grid item xs={2}>
+							<Typography variant="body1" color="secondary" textAlign="left">
+								Welcome to the game {username}
+							</Typography>
+							<Typography variant="body1" color="success.main">
+								{onlineCount} - Online Users
+							</Typography>
+						</Grid>
+					</Grid>
+					<Grid item xs={2}>
+						<CopyToClipboard text={game.code} onCopy={() => setCopied(true)}>
+							<Button
+								variant="contained"
+								sx={{ ml: "2em" }}
+								onClick={() => setCopied(true)}
+							>
+								<Typography ref={textAreaRef} color="text" variant="body1">
+									{game.code}
+								</Typography>
+							</Button>
+						</CopyToClipboard>
+						<Typography color="primary" variant="body2">
+							Click to share the game code
+						</Typography>
+						{copied ? (
+							<Typography color="error" variant="body2" textAlign="center">
+								copied!
+							</Typography>
+						) : null}
+					</Grid>
+					<Grid item xs={4} textAlign="center" sx={{ m: 6 }}>
+						<img src={game.image} height="480px" alt={game.title} />
+					</Grid>
+					<Grid
+						item
+						xs={4}
+						textAlign="center"
+						sx={{ mr: "auto", ml: "auto", maxWidth: "50em" }}
+					>
+						<Typography variant="h4" sx={{ color: "yellow" }}>
+							{game.description}
+						</Typography>
+					</Grid>
+					<Divider />
+					<Grid
+						item
+						xs={4}
+						sx={{
+							mb: "10em",
+							mt: "5em",
+							mr: "auto",
+							ml: "auto",
+							maxWidth: "50em",
+						}}
+					>
+						<Typography variant="h4">how to play</Typography>
 						<Divider />
-						<Box
-							sx={{
-								mb: "10em",
-								mt: "5em",
-								mr: "auto",
-								ml: "auto",
-								maxWidth: "50em",
-							}}
-						>
-							<Typography variant="h6">{game.playing_instructions}</Typography>
-							<Typography variant="h4">{game.code}</Typography>
-						</Box>
-						<Box
-							display="flex"
-							justifyContent="center"
-							alignItems="center"
-							sx={{ mb: "5em" }}
-						>
-							<Vote energiser={game} />
-						</Box>
+						<Typography variant="h6" sx={{ color: "white" }}>
+							{game.playing_instructions}
+						</Typography>
+					</Grid>
+					<Grid item xs={3} sx={{ mb: "5em" }}>
+						<Vote energiser={game} />
 					</Grid>
 				</Grid>
 			</Box>
